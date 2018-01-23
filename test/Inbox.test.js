8f3430d59687ef660365f0760d9255ac520298c1
Leonardo Souza <leonardocsouza@gmail.com>
2018-01-22 21:18:53 -0500

    add contract deployment step

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -11,17 +11,23 @@ const Web3 = require('web3');
 // lowercase first letter (w) because this is the instance
 const web3 = new Web3(ganache.provider());
 
+const { interface, bytecode} = require('../compile');
+
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['hello world'] })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(accounts);
+    console.log(inbox);
   });
 });
